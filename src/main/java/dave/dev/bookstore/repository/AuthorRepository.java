7417c458ffb8f70a6387c0747cbfcdc7f682df4a
John David Dela Cruz <johndaviddc1@gmail.com>
2023-10-18 10:54:38 +0800

    added author repository

---
+++ src/main/java/dave/dev/bookstore/repository/AuthorRepository.java
@@ -0,0 +1,9 @@
+package dave.dev.bookstore.repository;
+
+import dave.dev.bookstore.model.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AuthorRepository extends JpaRepository<Author, Long> {
+}
