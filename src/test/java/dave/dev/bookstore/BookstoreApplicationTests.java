fa3fb75dc327e6d4cdb77411b641396276d8eaf4
John David Dela Cruz <johndaviddc1@gmail.com>
2023-10-18 10:14:09 +0800

    Bookstore API

---
+++ src/test/java/dave/dev/bookstore/BookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package dave.dev.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
