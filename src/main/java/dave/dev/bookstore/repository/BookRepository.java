bbafb3fdd750ba1b784ce356f531e3f6fa20adb1
John David Dela Cruz <johndaviddc1@gmail.com>
2023-10-18 10:53:07 +0800

    added book repository

---
+++ src/main/java/dave/dev/bookstore/repository/BookRepository.java
@@ -0,0 +1,9 @@
+package dave.dev.bookstore.repository;
+
+import dave.dev.bookstore.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+}
