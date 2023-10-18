abfd0d079146b9b20129a6ccd1db3f55a834b4e0
John David Dela Cruz <johndaviddc1@gmail.com>
2023-10-18 10:58:18 +0800

    added review repository

---
+++ src/main/java/dave/dev/bookstore/repository/ReviewRepository.java
@@ -0,0 +1,9 @@
+package dave.dev.bookstore.repository;
+
+import dave.dev.bookstore.model.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+}
