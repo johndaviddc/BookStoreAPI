90b866e6d91de6ac5a4e31d79f7c790f66f90b26
John David Dela Cruz <johndaviddc1@gmail.com>
2023-10-19 09:37:49 +0800

    added get mapping in author controller

--- src/main/java/dave/dev/bookstore/controller/AuthorController.java
+++ src/main/java/dave/dev/bookstore/controller/AuthorController.java
@@ -1,7 +1,37 @@
 package dave.dev.bookstore.controller;
 
+import dave.dev.bookstore.model.Author;
+import dave.dev.bookstore.service.AuthorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/api/authors")
 public class AuthorController {
+    private final AuthorService authorService;
+
+    @Autowired
+    public AuthorController(AuthorService authorService) {
+        this.authorService = authorService;
+    }
+
+    @GetMapping
+    public List<Author> getAllAuthors() {
+        return authorService.getAllAuthors();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Author> getAuthorById(@PathVariable Long id) {
+        Author author = authorService.getAuthorById(id);
+        if (author != null) {
+            return ResponseEntity.ok(author);
+        }
+        return ResponseEntity.notFound().build();
+    }
 }
