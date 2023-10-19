fafcae7627ba3a983fb3129ba5ecccdfa6c96fca
John David Dela Cruz <johndaviddc1@gmail.com>
2023-10-19 09:39:44 +0800

    added post mapping

--- src/main/java/dave/dev/bookstore/controller/AuthorController.java
+++ src/main/java/dave/dev/bookstore/controller/AuthorController.java
@@ -4,10 +4,7 @@ import dave.dev.bookstore.model.Author;
 import dave.dev.bookstore.service.AuthorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -33,5 +30,11 @@ public class AuthorController {
             return ResponseEntity.ok(author);
         }
         return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<Author> createAuthor(@RequestBody Author author) {
+        Author createdAuthor = authorService.createAuthor(author);
+        return ResponseEntity.status(201).body(createdAuthor);
     }
 }
