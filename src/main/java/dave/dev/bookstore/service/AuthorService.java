62057321cf65c834f600151481f3725f851497c3
John David Dela Cruz <johndaviddc1@gmail.com>
2023-10-19 09:13:23 +0800

    added author service

---
+++ src/main/java/dave/dev/bookstore/service/AuthorService.java
@@ -0,0 +1,38 @@
+package dave.dev.bookstore.service;
+
+import dave.dev.bookstore.model.Author;
+import dave.dev.bookstore.repository.AuthorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AuthorService {
+    private final AuthorRepository authorRepository;
+
+    @Autowired
+    public AuthorService(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    public List<Author> getAllAuthors() {
+        return authorRepository.findAll();
+    }
+
+    public Author getAuthorById(Long id) {
+        return authorRepository.findById(id).orElse(null);
+    }
+
+    public Author createAuthor(Author author) {
+        return authorRepository.save(author);
+    }
+
+    public Author updateAuthor(Long id, Author updatedAuthor) {
+        if (authorRepository.existsById(id)) {
+            updatedAuthor.setId(id);
+            return authorRepository.save(updatedAuthor);
+        }
+        return null;
+    }
+}
