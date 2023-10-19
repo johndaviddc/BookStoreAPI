5730d8862954cfac59424e85c401256041b339be
John David Dela Cruz <johndaviddc1@gmail.com>
2023-10-19 09:22:08 +0800

    added book controller

---
+++ src/main/java/dave/dev/bookstore/controller/BookController.java
@@ -0,0 +1,20 @@
+package dave.dev.bookstore.controller;
+
+import dave.dev.bookstore.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/books")
+public class BookController {
+    private final BookService bookService;
+
+    @Autowired
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping
+}
