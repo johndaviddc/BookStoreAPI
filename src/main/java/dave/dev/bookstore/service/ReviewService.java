832bb391337c611d80ebca66c349aa2bf16e7ddc
John David Dela Cruz <johndaviddc1@gmail.com>
2023-10-19 09:19:31 +0800

    added review service

---
+++ src/main/java/dave/dev/bookstore/service/ReviewService.java
@@ -0,0 +1,42 @@
+package dave.dev.bookstore.service;
+
+import dave.dev.bookstore.model.Review;
+import dave.dev.bookstore.repository.ReviewRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ReviewService {
+    private final ReviewRepository reviewRepository;
+
+    @Autowired
+    public ReviewService(ReviewRepository reviewRepository) {
+        this.reviewRepository = reviewRepository;
+    }
+
+    public List<Review> getAllReviews() {
+        return reviewRepository.findAll();
+    }
+
+    public Review getReviewById(Long id) {
+        return reviewRepository.findById(id).orElse(null);
+    }
+
+    public Review createReview(Review review) {
+        return reviewRepository.save(review);
+    }
+
+    public Review updateReview(Long id, Review updatedReview) {
+        if (reviewRepository.existsById(id)) {
+            updatedReview.setId(id);
+            return reviewRepository.save(updatedReview);
+        }
+        return null;
+    }
+
+    public void deleteReview(Long id) {
+        reviewRepository.deleteById(id);
+    }
+}
