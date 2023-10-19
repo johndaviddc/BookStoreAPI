cd86a30bd22eae75a3de3f0e3348a5ac6434b374
John David Dela Cruz <johndaviddc1@gmail.com>
2023-10-19 09:24:51 +0800

    added get mapping

--- src/main/java/dave/dev/bookstore/controller/BookController.java
+++ src/main/java/dave/dev/bookstore/controller/BookController.java
@@ -1,10 +1,15 @@
 package dave.dev.bookstore.controller;
 
+import dave.dev.bookstore.model.Book;
 import dave.dev.bookstore.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/books")
@@ -17,4 +22,16 @@ public class BookController {
     }
 
     @GetMapping
+    public List<Book> getAllBooks() {
+        return bookService.getAllBooks();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Book> getBookById(@PathVariable Long id) {
+        Book book = bookService.getBookById(id);
+        if (book != null) {
+            return ResponseEntity.ok(book);
+        }
+        return ResponseEntity.notFound().build();
+    }
 }
