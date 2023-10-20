cae4a419bfdb054e6668b09077941c3cec88ab63
John David Dela Cruz <johndaviddc1@gmail.com>
2023-10-20 13:59:32 +0800

    added get mapping

--- src/main/java/dave/dev/bookstore/controller/ReviewController.java
+++ src/main/java/dave/dev/bookstore/controller/ReviewController.java
@@ -1,7 +1,11 @@
 package dave.dev.bookstore.controller;
 
+import dave.dev.bookstore.model.Review;
 import dave.dev.bookstore.service.ReviewService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,5 +19,12 @@ public class ReviewController {
         this.reviewService = reviewService;
     }
 
-    
+    @GetMapping
+    public ResponseEntity<Review> getReviewById(@PathVariable Long id) {
+        Review review = reviewService.getReviewById(id);
+        if (review != null) {
+            return ResponseEntity.ok(review);
+        }
+        return ResponseEntity.notFound().build();
+    }
 }
