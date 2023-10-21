ca110c40013e43fdbb3a4e0ce7e675805b319517
John David Dela Cruz <johndaviddc1@gmail.com>
2023-10-21 13:55:19 +0800

    added method argument not valid exception

--- src/main/java/dave/dev/bookstore/exception/GlobalExceptionHandler.java
+++ src/main/java/dave/dev/bookstore/exception/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package dave.dev.bookstore.exception;
 import org.springframework.boot.context.config.ConfigDataResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -26,5 +27,13 @@ public class GlobalExceptionHandler {
         details.add(ex.getMessage());
         ApiError error = new ApiError(HttpStatus.NOT_FOUND, "Resource Not Found", details);
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiError> handleValidationException(MethodArgumentNotValidException ex) {
+        List<String> details = new ArrayList<>();
+        ex.getBindingResult().getFieldErrors().forEach(error -> details.add(error.getDefaultMessage()));
+        ApiError error = new ApiError(HttpStatus.BAD_REQUEST, "Validation Error", details);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
 }
